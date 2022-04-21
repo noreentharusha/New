3e6d367ef2b36f540ee8e8211a066e1e28e4b784
Noreen Tharusha <noreentharusha@gmail.com>
2022-04-21 22:00:44 +0530

    Add files via upload

--- Server/index.js
+++ Server/index.js
@@ -23,6 +23,7 @@ app.post('/api/register', async (req, res) => {
         })
 
 	res.json({ status: 'ok' })
+    
 	} catch (err) {
     console.log(err)
     res.json({ status: 'error', error: 'Duplicate email' })
@@ -35,32 +36,66 @@ app.post('/api/register', async (req, res) => {
 
 
 app.post ('/api/login', async (req, res) => {
-    
-    
-        const user = await User.findOne({
+    const user = await User.findOne({
             
             email: req.body.email,
             password: req.body.password,
 
         })
 
-       if (user){
-           const token =jwt.sign({
-               name: user.name,
-               email: user.email,
-           },
-           'secret123'
-           )
+    if (user){
+       const token =jwt.sign({
+         name:user.name,
+         email:user.email,
+       },'secret123'
+       )
 
 
-           return res.json({status:'ok', user: token })
-
-       }else{
-           
-        return res.json({status:'error', user: false })
-       }
+      return res.json({status:'ok', user: token })
+    }else{
+      return res.json({status:'error', user: false })
+    }
         
 })
+
+app.get ('/read', async (req, res) => {
+     User.find({}, (err,result) =>{
+        if (err){
+            return res.send(err)
+          }else{
+            return res.send(result)
+          }
+     })
+    
+        
+})
+
+
+
+
+app.put ('/update', async (req, res) => {
+ const newPassword = req.body.newPassword
+ const id= req.body.id
+
+ try{
+    await User.findById(id,(error,userToUpdate) =>{
+      userToUpdate.password =newPassword
+      userToUpdate.save()
+
+    })
+ }catch(err){
+   console.log(err)
+ }
+ res.send('updated')
+     
+})
+
+app.delete ('/delete/:id', async (req, res) => {
+ 
+      const id =req.params.id
+      await User.findByIdAndRemove(id).exec();
+      res.send('item deleted')
+ })
 
 app.listen(1337, () => {
     console.log('Server started on 1337')
